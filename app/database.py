e8dd61d330ab33894a1460eb8c7e96ed7d8d2182
Ethan Swan <eswan18@rocketmail.com>
2022-03-26 23:24:38 -0500

    Update database module for Heroku usage.

--- app/database.py
+++ app/database.py
@@ -1,8 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://eswan18@localhost/ethan_runs'
+if 'DATABASE_URL' in os.environ:
+    DATABASE_URL = os.environ['DATABASE_URL']
+else:
+    DATABASE_URL = 'postgresql://eswan18@localhost/ethan_runs'
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
