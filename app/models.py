6e057d08367772f01190abee8af4e64807d657b1
Ethan Swan <eswan18@rocketmail.com>
2022-03-27 22:18:41 -0500

    Update sqlalchemy models to match db schema.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Column, Integer, Float, String, Date
+from sqlalchemy import Column, Date, Integer, Float, Text, text
 from sqlalchemy.dialects.postgresql import UUID
 
 from .database import Base
@@ -9,7 +9,11 @@ from .database import Base
 class Activity(Base):
     __tablename__ = 'activity_fact'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(
+        Integer,
+        primary_key=True,
+        server_default=text("nextval('activity_fact_id_seq'::regclass)"),
+    )
     date_submitted = Column(Date)
     workout_date = Column(Date)
     avg_speed_in_mi_per_h = Column(Float)
@@ -21,16 +25,16 @@ class Activity(Base):
     workout_time_in_seconds = Column(Integer)
     avg_pace_in_min_per_mi = Column(Float)
     max_pace_in_min_per_mi = Column(Float)
-    activity_type = Column(String)
-    notes = Column(String)
-    source = Column(String)
-    link = Column(String)
+    activity_type = Column(Text)
+    notes = Column(Text)
+    source = Column(Text)
+    link = Column(Text)
 
 
 class User(Base):
     __tablename__ = 'users'
 
     id: UUID = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    username: str = Column(String, unique=True, nullable=False)
-    email: str = Column(String, unique=True, nullable=False)
-    pw_hash: str = Column(String, nullable=False)
+    username: str = Column(Text, unique=True, nullable=False)
+    email: str = Column(Text, unique=True, nullable=False)
+    pw_hash: str = Column(Text, nullable=False)
