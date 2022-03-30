0e59c84de9b661297538914afed354b184c78e88
Ethan Swan <eswan18@rocketmail.com>
2022-03-29 22:11:11 -0500

    Create mostly-finished CSV upload script.

--- scripts/upload_activities.py
+++ scripts/upload_activities.py
@@ -8,7 +8,22 @@ import requests
 
 from app.schemas.activity import ActivityIn
 
-DATE_FORMAT = '%b. %d, %Y'
+
+def parse_date(d: str) -> datetime.date:
+    '''
+    Date values are stored in a non-standard format.
+
+    All months are appreviated with three letters (e.g. Dec.) except September (Sept.)
+    and months with names five letters and shorter (e.g. March), which don't even have
+    a period in them.
+    '''
+    d = d.replace('.', '')
+    # Keep only the first three letters of the month
+    # (drop 't' from Sept, 'ch' from March)
+    month, rest = d.split(' ', 1)
+    d = month[:3] + ' ' + rest
+    date = datetime.strptime(d, '%b %d, %Y').date()
+    return date
 
 
 if __name__ == '__main__':
@@ -37,14 +52,15 @@ if __name__ == '__main__':
     # Create activities using this data and post them.
     for row in rows:
         row_dict = dict(zip(headers, row))
-        # Date values are spelled out.
+        # Date values must be fixed.
         for key in ('date_submitted', 'workout_date'):
-            date = datetime.strptime(row_dict[key], DATE_FORMAT)
+            date = parse_date(row_dict[key])
             row_dict[key] = date.strftime('%Y-%m-%d')
+        # Some numeric values come in as empty strings but need to be null.
+        for key in ('avg_heart_rate', 'steps'):
+            if row_dict[key] == '':
+                row_dict[key] = None
         j_str = json.dumps(row_dict)
-        print(j_str)
         activity = ActivityIn.parse_raw(j_str)
-        # activity = ActivityIn(**row_dict)
-        print(activity)
-        #response = requests.post(f'{url}/activity', data=activity.json())
-        #print(response.status_code)
+        response = requests.post(f'{url}/activity', data=activity.json())
+        print(response.status_code)
